a24ba75c83d6ea6830968ef358fe4ba7dc2ddf5c
ambitionyu <939649107@qq.com>
2019-10-26 13:42:54 +0800

    单例模式

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import {Factory} from "./factory/factory";
-let water = new Factory("Water","农夫山泉",2)
-let coffe = new Factory("Coffe","雀巢",3)
-water.sell()
-coffe.sell()
-
+//简单工厂模式
+// import {Factory} from "./factory/factory";
+// let water = new Factory("Water","农夫山泉",2)
+// let coffe = new Factory("Coffe","雀巢",3)
+// water.sell()
+// coffe.sell()
+//单例模式
+// import  {Singleton} from "./singleton/singleton";
+// let objFalse = new Singleton()//false
+// let obj1 = Singleton.instance()
+// let obj2 = Singleton.instance()
+// console.log(obj1===obj2,obj1===objFalse)
